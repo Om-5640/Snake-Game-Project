685db80fbeab63207a42c5e09682f69c6179823e
Om-5640 <ompandya1234q@gmail.com>
2025-02-12 10:22:48 +0530

    Updated snake_game.cpp

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,53 +1,9 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <thread>
-#include <chrono>
-
-#ifdef _WIN32
-    #include <conio.h>   // Windows keyboard input
-    #include <windows.h> // Windows system commands
-    #define CLEAR_SCREEN "cls"
-    void sleep_ms(int ms) { Sleep(ms); }
-#else
-    #include <termios.h>
-    #include <unistd.h>
-    #define CLEAR_SCREEN "clear"
-    void sleep_ms(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
-
-    int _kbhit() {
-        struct termios oldt, newt;
-        int ch;
-        int oldf;
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-        ch = getchar();
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        fcntl(STDIN_FILENO, F_SETFL, oldf);
-        if (ch != EOF) {
-            ungetc(ch, stdin);
-            return 1;
-        }
-        return 0;
-    }
-
-    int _getch() {
-        struct termios oldt, newt;
-        int ch;
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        ch = getchar();
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        return ch;
-    }
-#endif
 
 using namespace std;
 
@@ -56,34 +12,39 @@ enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 class SnakeGame {
 private:
     bool gameOver;
-    const int width;
-    const int height;
+    int width, height;
     int x, y, foodX, foodY, score;
-    int speed, baseSpeed;
+    int speed;
+    int level;
+    char foodType;
     Direction dir;
     vector<pair<int, int>> snake;
     vector<pair<int, int>> obstacles;
-    vector<pair<int, int>> portals;
-    bool hasObstacles, hasPortals;
 
 public:
-    SnakeGame(int w, int h) 
-        : width(w), height(h), gameOver(false), score(0), dir(STOP), 
-          hasObstacles(false), hasPortals(false) {}
+    SnakeGame() : gameOver(false), score(0), dir(STOP) {}
 
-    void selectDifficulty() {
-        char choice;
-        cout << "Select Difficulty - (E)asy, (M)edium, (H)ard: ";
-        cin >> choice;
+    void setupGame() {
+        cout << "Enter Grid Height: ";
+        cin >> height;
+        cout << "Enter Grid Width: ";
+        cin >> width;
 
-        if (choice == 'E' || choice == 'e') baseSpeed = 150;
-        else if (choice == 'M' || choice == 'm') baseSpeed = 100;
-        else baseSpeed = 60;
+        cout << "Choose difficulty level:\n";
+        cout << "1 -(Easy)\n2 -(Medium)\n3 -(Hard)\n";
+        cout << "Enter level: ";
+        cin >> level;
 
-        speed = baseSpeed;
+        if (level == 1) speed = 110;
+        else if (level == 2) speed = 60;
+        else if (level == 3) speed = 20;
+        else speed = 110;  
+
+        resetGame();
     }
 
     void resetGame() {
+        system("cls");
         gameOver = false;
         score = 0;
         dir = STOP;
@@ -91,88 +52,115 @@ public:
         y = height / 2;
         snake.clear();
         snake.push_back({x, y});
-        foodX = rand() % width;
-        foodY = rand() % height;
+        snake.push_back({x - 1, y});
+        
         obstacles.clear();
-        portals.clear();
+        generateObstacles();
+        spawnFood();
+    }
 
-        selectDifficulty(); 
+    void generateObstacles() {
+        int numObstacles = (width * height) / (level == 1 ? 70 : (level == 2 ? 60 : 40));
 
-        char obstacleChoice;
-        cout << "Do you want to enable obstacles? (y/n): ";
-        cin >> obstacleChoice;
-        hasObstacles = (obstacleChoice == 'y' || obstacleChoice == 'Y');
+        for (int i = 0; i < numObstacles; i++) {
+            int obsX, obsY;
+            do {
+                obsX = rand() % width;
+                obsY = rand() % height;
+            } while ((obsX == x && obsY == y) || (obsX == foodX && obsY == foodY));
 
-        if (hasObstacles) {
-            int numObstacles = (width * height) / 60;
-            for (int i = 0; i < numObstacles; i++) {
-                int obsX, obsY;
-                do {
-                    obsX = rand() % width;
-                    obsY = rand() % height;
-                } while ((obsX == x && obsY == y) || (obsX == foodX && obsY == foodY));
-                obstacles.push_back({obsX, obsY});
-            }
-        }
-
-        char portalChoice;
-        cout << "Do you want to enable teleportation portals? (y/n): ";
-        cin >> portalChoice;
-        hasPortals = (portalChoice == 'y' || portalChoice == 'Y');
-
-        if (hasPortals) {
-            for (int i = 0; i < 2; i++) {
-                int portalX, portalY;
-                do {
-                    portalX = rand() % width;
-                    portalY = rand() % height;
-                } while ((portalX == x && portalY == y) || (portalX == foodX && portalY == foodY));
-                portals.push_back({portalX, portalY});
-            }
+            obstacles.push_back({obsX, obsY});
         }
     }
 
-    void draw() {
-        cout << "\033[H";  // Move cursor to top left without clearing the screen
+    void spawnFood() {
+        do {
+            foodX = rand() % width;
+            foodY = rand() % height;
+        } while (foodCollides());
 
-        vector<string> buffer(height + 2, string(width + 2, ' '));
+        int foodChance = rand() % 100;
+        foodType = (foodChance < 70) ? 'F' : (foodChance < 90) ? 'S' : 'M';
+    }
 
-        for (int i = 0; i < width + 2; i++) {
-            buffer[0][i] = '#';
-            buffer[height + 1][i] = '#';
-        }
-
-        for (int i = 1; i <= height; i++) {
-            buffer[i][0] = '#';
-            buffer[i][width + 1] = '#';
-        }
-
+    bool foodCollides() {
         for (auto part : snake) {
-            buffer[part.second + 1][part.first + 1] = '*';
+            if (foodX == part.first && foodY == part.second)
+                return true;
         }
-
-        buffer[y + 1][x + 1] = '*';
-        buffer[foodY + 1][foodX + 1] = 'F';
-
         for (auto obs : obstacles) {
-            buffer[obs.second + 1][obs.first + 1] = '#';
+            if (foodX == obs.first && foodY == obs.second)
+                return true;
         }
-
-        if (hasPortals) {
-            buffer[portals[0].second + 1][portals[0].first + 1] = 'O';
-            buffer[portals[1].second + 1][portals[1].first + 1] = 'O';
-        }
-
-        for (const auto& line : buffer) {
-            cout << line << endl;
-        }
-
-        cout << "Score: " << score << endl;
+        return false;
     }
 
-    void input() {
-        if (_kbhit()) {
-            switch (_getch()) {
+  void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void hideCursor() {
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+void draw() {
+    gotoxy(0, 0);  
+
+    vector<string> buffer(height + 2, string(width + 2, ' '));
+
+    for (int i = 0; i < width + 2; i++) {
+        buffer[0][i] = '#';
+        buffer[height + 1][i] = '#';
+    }
+
+    for (int i = 1; i <= height; i++) {
+        buffer[i][0] = '#';
+        buffer[i][width + 1] = '#';
+    }
+
+    for (auto part : snake) {
+        buffer[part.second + 1][part.first + 1] = 'o';
+    }
+
+    buffer[y + 1][x + 1] = 'O';
+    buffer[foodY + 1][foodX + 1] = foodType;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    
+
+    for (auto obs : obstacles) {
+        buffer[obs.second + 1][obs.first + 1] = 'X';
+    }
+
+    for (const auto& line : buffer) {
+        cout << line << endl;
+    }
+
+    cout << "Score: " << score << "  (F = +10, S = +30, M = +50)" << endl;
+}
+
+
+   void input() {
+    if (_kbhit()) {
+        Direction olddir = dir;
+        char key = _getch();
+
+        if (key == -32) {
+            key = _getch();
+            switch (key) {
+                case 72: if (dir != DOWN) dir = UP; break;
+                case 80: if (dir != UP) dir = DOWN; break;
+                case 75: if (dir != RIGHT) dir = LEFT; break;
+                case 77: if (dir != LEFT) dir = RIGHT; break;
+            }
+        } else {
+            switch (key) {
                 case 'a': if (dir != RIGHT) dir = LEFT; break;
                 case 'd': if (dir != LEFT) dir = RIGHT; break;
                 case 'w': if (dir != DOWN) dir = UP; break;
@@ -181,75 +169,95 @@ public:
             }
         }
     }
+}
+
 
     void logic() {
+        if (dir == STOP) return;
+
         snake.insert(snake.begin(), {x, y});
 
         if (x == foodX && y == foodY) {
+           int growth = 1;  
+        if (foodType == 'F') {
             score += 10;
-            foodX = rand() % width;
-            foodY = rand() % height;
-        } else {
-            snake.pop_back();
+            growth = 1;
+        } else if (foodType == 'S') {
+            score += 30;
+            growth = 3;
+        } else if (foodType == 'M') {
+            score += 50;
+            growth = 5;
         }
+        
+        
+        for (int i = 0; i < growth-1; i++) {
+            snake.push_back(snake.back());
+        }
+
+        spawnFood();}  
+     else {
+        snake.pop_back();  
+    }
+
 
         switch (dir) {
             case LEFT: x--; break;
             case RIGHT: x++; break;
             case UP: y--; break;
             case DOWN: y++; break;
+            default: break;
         }
 
-        if (x < 0 || x >= width || y < 0 || y >= height) {
+        if (x < 0 || x >= width || y < 0 || y >= height)
             gameOver = true;
-            return;
-        }
 
-        for (int i = 1; i < snake.size(); i++) {
+        for (size_t i = 1; i < snake.size(); i++) {
             if (snake[i].first == x && snake[i].second == y) {
                 gameOver = true;
-                return;
+                break;
             }
         }
 
-        if (hasObstacles) {
-            for (auto obs : obstacles) {
-                if (x == obs.first && y == obs.second) {
-                    gameOver = true;
-                    return;
-                }
+        for (auto obs : obstacles) {
+            if (x == obs.first && y == obs.second) {
+                gameOver = true;
+                break;
             }
-        }
-
-        if (hasPortals && ((x == portals[0].first && y == portals[0].second) ||
-                           (x == portals[1].first && y == portals[1].second))) {
-            auto otherPortal = (x == portals[0].first && y == portals[0].second) ? portals[1] : portals[0];
-            x = otherPortal.first;
-            y = otherPortal.second;
         }
     }
 
     void run() {
+        int maxx = 0;
+        setupGame();
         while (true) {
-            resetGame();
             while (!gameOver) {
                 draw();
                 input();
                 logic();
-                sleep_ms(speed);
+                Sleep(speed);
             }
-            cout << "Game Over! Score: " << score << endl;
-            char choice;
-            cout << "Press 'R' to restart or any other key to exit: ";
+               maxx = max(maxx, score);
+            
+          cout << "Game Over!\nYour score: " << score << "\nMax Score: " << maxx << endl;
+            string choice;
+            cout << "Press 'r' to restart or any other key to exit: ";
             cin >> choice;
-            if (choice != 'R' && choice != 'r') break;
+            
+            if (choice == "r" || choice=="R") {
+                setupGame();
+            } else {
+    
+                cout << " Game Over! Your adventure has ended. \n";
+                break;
+            }
         }
     }
 };
 
 int main() {
-    srand(time(0));
-    SnakeGame game(30, 20);
+    srand(static_cast<unsigned>(time(0)));
+    SnakeGame game;
     game.run();
     return 0;
 }
